3f9d020a0a8ef573febf497238383400e6b9b36f
Shiferex <49259112+Shiferex@users.noreply.github.com>
2019-09-17 19:26:11 -0300

    Fix error en funcion de cambio de diapositiva. Selector con un espacio de mas

--- assets/scripts/CambioDeDiapo.js
+++ assets/scripts/CambioDeDiapo.js
@@ -1,12 +1,12 @@
 function CambiarDiapostiva(id){
-	var dataDiapo =$('#' + id +' .diapositiva');
+	var dataDiapo =$('#' + id +'.diapositiva');
 	var texto = dataDiapo.children('.texto').val();
 	var opcion1Texto = dataDiapo.children('.opcion1Texto').val();
 	var opcion1Id = dataDiapo.children('.opcion1Id').val();
 	var opcion2Texto = dataDiapo.children('.opcion2Texto').val();
 	var opcion2Id = dataDiapo.children('.opcion2Id').val();
 	var srcName = dataDiapo.children('.srcName').val();
-	var EsFinal = dataDiapo.children('.EsFinal').val();
+	var esFinal = dataDiapo.children('.EsFinal').val();
 	if(esFinal == true){
 		//ALGO TENEMOS QUE HACER ACA
 	}
