07009d15ed2d62789d7452319d3dcd2428b76689
Shiferex <49259112+Shiferex@users.noreply.github.com>
2019-09-17 00:29:38 -0300

    Funcion CambioDeDiapo. \n Dato curioso: 7% de los adultos americanos piensan que la leche chocolatada viene de vacas marrones

---
+++ assets/scripts/CambioDeDiapo.js
@@ -0,0 +1,22 @@
+function CambiarDiapostiva(id){
+	var dataDiapo =$('#' + id +' .diapositiva');
+	var texto = dataDiapo.children('.texto').val();
+	var opcion1Texto = dataDiapo.children('.opcion1Texto').val();
+	var opcion1Id = dataDiapo.children('.opcion1Id').val();
+	var opcion2Texto = dataDiapo.children('.opcion2Texto').val();
+	var opcion2Id = dataDiapo.children('.opcion2Id').val();
+	var srcName = dataDiapo.children('.srcName').val();
+	var EsFinal = dataDiapo.children('.EsFinal').val();
+	if(esFinal == true){
+		//ALGO TENEMOS QUE HACER ACA
+	}
+	else{
+		$('#tagTexto').html(texto);
+		$('#tagOpcion1').html(opcion1Texto);
+		$('#tagOpcion1').val(opcion1Id);
+		$('#tagOpcion2').html(opcion2Texto);
+		$('#tagOpcion2').val(opcion2Id);
+		//La imagen esta en css ver que tocar para cambiarla
+		$('#tagSrc').attr('src',srcName);
+	};
+}
