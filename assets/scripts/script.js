eee6935f3a10d01228261ad63ebdc3dbd8f047e0
Jota <juann.asd@gmail.com>
2019-09-19 20:34:59 -0300

    Agrego los events de los botones. Botones como los de la federaaaaaaaaaaaal

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,11 @@
-  $("#aceptar").click(function(){ $("#start").hide("slow");});
-  $("#cancelar").click(function(){
-     $("#start").css({"background-color":"#ff6b6b"}),
-     $(".second").hide("100"),
-     $(".first>p").text("GameOver");
-   }
-  );
+$("#aceptar").click(function(){ $("#start").hide("slow");});
+$("#cancelar").click(function(){
+	$("#start").css({"background-color":"#ff6b6b"}),
+	$(".second").hide("100"),
+	$(".first>p").text("GameOver");
+	}
+);
+$(function (){
+	$('#btnOpcion1').click(function(){CambiarDiapostiva($(this).val());});
+	$('#btnOpcion2').click(function(){CambiarDiapostiva($(this).val());});
+});
