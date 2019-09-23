23f9e734e5ea35cf948d0f3e68c21455c84b3eca
ramirobelmonte <ramirobelmon@gmail.com>
2019-09-22 21:30:46 -0300

    ahi

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,12 @@
-$("#aceptar").click(function(){ $("#start").hide("slow");});
+$("#aceptar").click(function(){
+	 $("#start").hide("slow"),
+	 $(".mainB").show();
+ }
+);
+
 $("#cancelar").click(function(){
 	$("#start").css({"background-color":"#ff6b6b"}),
 	$(".second").hide("100"),
-	$(".tyc").hide("100"),
 	$(".first>p").text("GameOver");
 	}
 );
